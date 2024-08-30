b82f6d2e3ce527cb64a4e6e80f0fc5aefdae8551
Lucas Fernando Pinto <40510593+lucasfernandopinto@users.noreply.github.com>
2024-08-30 22:38:26 +0000

    criação das classes

--- doggie/saude_animal/models.py
+++ doggie/saude_animal/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Documento(models.Model):
+    tipo = models.CharField(max_length=50)
+    arquivo = models.FileField(upload_to="arquivo")
+    id_prontuario = models.ForeignKey(Prontuario, on_delete=CASCADE)
+
+class Exame(models.Model):
+    tipo = models.CharField(max_length=50)
+    data = models.DateField()
+    resultado = models.CharField(max_length=200)
+    id_animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
+    id_veterinaria = models.ForeignKey(Veterinaria, on_delete=CASCADE)
+    
+class Monitoramento(models.Model):
+    batimentos = models.IntegerField()
+    temperatura = models.DecimalField(max_digits=5, decimal_places=2)
+    data = models.DateField()
+    id_animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
+
+class Notificacao(models.Model):
+    tipo = models.CharField(max_length=50)
+    mensagem = models.CharField(max_length=200)
+    data = models.DateField()
+    id_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE) 
