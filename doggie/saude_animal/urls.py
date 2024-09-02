99c5623d6a748918e26b09b9bbbb8f7e6f1798f1
Kainã Magdiel <114326061+fushisuno@users.noreply.github.com>
2024-09-01 22:31:48 -0300

    Update urls.py

--- doggie/saude_animal/urls.py
+++ doggie/saude_animal/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import DonoView, VeterinariaView
+from .views import *
 
 urlpatterns = [
   #Criando as rotas de cada pagina vista pelo usuario
   path('dono', DonoView.as_view()),
-  path('veterinaria', VeterinariaView.as_view())
+  path('dono/<int:pk>/', DonoReadUpdateDeleteView.as_view(), name='dono-detail'),
+  path('veterinaria', VeterinariaView.as_view()),
+  path('veterinaria/<int:pk>/', VeterinariaReadUpdateDeleteView.as_view(), name='veterinaria-detail')
 ]
